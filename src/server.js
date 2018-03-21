37542441b01077df1c87c7d43a9dcfeba82e2fab
Neha Ambasta <nehaambasta08@gmail.com>
2018-03-21 11:45:04 +0530

    changes done for routing

--- src/server.js
+++ src/server.js
@@ -12,16 +12,11 @@ const forceSSL = function() {
       next();
     }
   }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceSSL());
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+  // define the folder that will be used for static assets
+app.use(Express.static(path.join(__dirname, '../public')));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+    response.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
